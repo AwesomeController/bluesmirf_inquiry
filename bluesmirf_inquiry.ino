43404f455ecadfe638aeb15a73708ada56b567c6
Alex Toumey <atoumey@gmail.com>
2012-03-03 16:51:33 -0500

    use SoftwareSerial so we can read BlueSmirf output through the Arduino serial monitor

--- bluesmirf_inquiry.ino
+++ bluesmirf_inquiry.ino
@@ -1,49 +1,78 @@
+#include <SoftwareSerial.h>
+
 int BOARD_LED = 13;
+
+SoftwareSerial smirfSerial(2, 3);
 
 bool configured = false;
 
 void setup() {
-    Serial.begin(115200);
+    smirfSerial.begin(9600);
+    Serial.begin(9600);
 
-    delay(500);
+    delay(2000);
 }
+
+char readValue[50];
 
 void loop() {
     if (!configured) {
-        enterCommandMode();
+        //enterCommandMode();
+        //changeBaudRate();
         // enterMasterMode();
         // discoverWiimotes();
-        connectToWiimote();
+        //connectToWiimote();
         configured = true;
     }
+    
+    
+		if (smirfSerial.available())
+				Serial.print((char)smirfSerial.read());
+		if (Serial.available())
+				smirfSerial.print((char)Serial.read());
+    
+//    int ii = 0;
+//    
+//    while(Serial.available()) {
+//        readValue[ii++] = (char)Serial.read();
+//    }
+//    if (ii > 0) {
+//        Serial.println(ii);
+//        smirfSerial.print(readValue);
+//    }
 }
 
 void enterCommandMode() {
-    Serial.print("$$$");
+    smirfSerial.print("$$$");
+    readData();
+}
+
+void changeBaudRate() {
+    smirfSerial.println("SU,9600");
     readData();
 }
 
 void enterMasterMode() {
-    Serial.println("SM,1");
+    smirfSerial.println("SM,1");
     readData();
 }
 
 void discoverWiimotes() {
-    Serial.println("I,10,000508");
+    smirfSerial.println("I,10,000508");
     readData();
 }
 
 void connectToWiimote() {
-    Serial.println("C")
+    smirfSerial.println("C");
     readData();
 }
 
 void readData() {
-    while(!Serial.available());
+    while(!smirfSerial.available());
     digitalWrite(BOARD_LED, HIGH);
     delay(200);
-    while(Serial.available()) {
-        Serial.read();
+    while(smirfSerial.available()) {
+        Serial.println(smirfSerial.read());
     }
     digitalWrite(BOARD_LED, LOW);
     delay(200);
